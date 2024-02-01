5628cf2677eb0c9cc3ac11657d1e980c5421aa2c
davis <zanxiaofeng@163.com>
2024-02-01 14:17:42 +0800

    update

--- src/main/java/zxf/java/easyrule/EasyRuleTests.java
+++ src/main/java/zxf/java/easyrule/EasyRuleTests.java
@@ -1,10 +1,9 @@
 package zxf.java.easyrule;
 
-import org.jeasy.rules.api.Facts;
-import org.jeasy.rules.api.Rules;
-import org.jeasy.rules.api.RulesEngine;
-import org.jeasy.rules.api.RulesEngineParameters;
+import org.jeasy.rules.api.*;
 import org.jeasy.rules.core.DefaultRulesEngine;
+import org.jeasy.rules.core.RuleBuilder;
+import org.jeasy.rules.mvel.MVELRule;
 import zxf.java.easyrule.rule.MyRule1;
 
 public class EasyRuleTests {
@@ -12,16 +11,33 @@ public class EasyRuleTests {
         Rules rules = new Rules();
         rules.register(new MyRule1());
 
+        Rule myRule2Fluent = new RuleBuilder()
+                .name("My rule 2")
+                .description("My rule 2")
+                .when(facts -> {
+                    System.out.println("MyRule2::when " + facts);
+                    return facts.get("name").equals("Davis");
+                }).then(facts -> {
+                    System.out.println("MyRule2::then " + facts);
+                }).build();
+        rules.register(myRule2Fluent);
+
+        Rule mvelRule = new MVELRule()
+                .name("My rule 3")
+                .description("My rule 3")
+                .when("age > 30")
+                .then("System.out.println(\"MyRule3::then \")");
+        rules.register(mvelRule);
+
         Facts facts = new Facts();
         facts.put("name", "Davis");
         facts.put("age", 40);
 
         RulesEngineParameters parameters = new RulesEngineParameters()
-                .priorityThreshold(10)
-                .skipOnFirstAppliedRule(true)
+                .priorityThreshold(10).skipOnFirstAppliedRule(true)
                 .skipOnFirstFailedRule(true)
                 .skipOnFirstNonTriggeredRule(true);
-        RulesEngine rulesEngine = new DefaultRulesEngine(parameters);
+        RulesEngine rulesEngine = new DefaultRulesEngine();
         rulesEngine.fire(rules, facts);
     }
 }
--- src/main/java/zxf/java/easyrule/Readme.md
+++ src/main/java/zxf/java/easyrule/Readme.md
@@ -5,14 +5,13 @@
 - org.jeasy.rules.api.Facts;
 - org.jeasy.rules.api.Rules;
 - org.jeasy.rules.api.RulesEngine;
-- org.jeasy.rules.core.DefaultRulesEngine;
 - org.jeasy.rules.api.RulesEngineParameters;
 - org.jeasy.rules.api.Rule
 - org.jeasy.rules.api.Fact<T>
+- org.jeasy.rules.core.DefaultRulesEngine;
+- org.jeasy.rules.core.RuleBuilder;
 - org.jeasy.rules.annotation.Rule;
 - org.jeasy.rules.annotation.Condition;
 - org.jeasy.rules.annotation.Fact;
 - org.jeasy.rules.annotation.Action;
-
-
-
+- org.jeasy.rules.mvel.MVELRule;
