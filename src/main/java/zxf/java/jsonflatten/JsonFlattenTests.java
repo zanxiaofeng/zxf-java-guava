13f79039ce92d95de6b3410d1c4caf0a6c67379d
Davis ZAN <zanxiaofeng@163.com>
2024-05-25 21:19:11 -0500

    update

--- src/main/java/zxf/java/jsonflatten/JsonFlattenTests.java
+++ src/main/java/zxf/java/jsonflatten/JsonFlattenTests.java
@@ -1,5 +1,6 @@
 package zxf.java.jsonflatten;
 
+import com.github.wnameless.json.flattener.FlattenMode;
 import com.github.wnameless.json.flattener.JsonFlattener;
 import com.github.wnameless.json.unflattener.JsonUnflattener;
 
@@ -7,12 +8,23 @@ import java.util.Map;
 
 public class JsonFlattenTests {
     public static void main(String[] args) {
-        String json = "{ \"a\" : { \"b\" : 1, \"c\": null, \"d\": [false, true] }, \"e\": \"f\", \"g\":2.3 }";
-        Map<String, Object> flattenJson = JsonFlattener.flattenAsMap(json);
-        System.out.println(flattenJson);
-        String jsonStr = JsonFlattener.flatten(json);
-        System.out.println(jsonStr);
-        String nestedJson = JsonUnflattener.unflatten(jsonStr);
-        System.out.println(nestedJson);
+        String unflattenJson = "{ \"a\" : { \"b\" : 1, \"c\": null, \"d\": [false, true] }, \"e\": \"f\", \"g\":2.3 }";
+        System.out.println(JsonFlattener.flatten(unflattenJson));
+
+        Map<String, Object> flattenJsonMap = JsonFlattener.flattenAsMap(unflattenJson);
+        printMap(flattenJsonMap);
+
+        System.out.println(JsonUnflattener.unflatten(flattenJsonMap));
+        System.out.println(new JsonUnflattener(flattenJsonMap).withFlattenMode(FlattenMode.KEEP_ARRAYS).unflatten());
+        printMap(JsonUnflattener.unflattenAsMap(flattenJsonMap));
+
+        String flattenJson = "{\"a.b\":1,\"a.c\":null,\"a.d[0]\":false,\"a.d[5]\":true,\"e\":\"f\",\"g\":2.3}";
+        System.out.println(JsonUnflattener.unflatten(flattenJson));
+        System.out.println(new JsonUnflattener(flattenJson).withFlattenMode(FlattenMode.KEEP_ARRAYS).unflatten());
+        printMap(JsonUnflattener.unflattenAsMap(flattenJson));
+    }
+
+    private static void printMap(Map<String, Object> map) {
+        System.out.println(map.getClass() + ":" + map);
     }
 }
