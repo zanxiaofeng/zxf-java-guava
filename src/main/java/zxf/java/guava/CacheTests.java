624baaf2dd655a1a1c4751c01bcdfff8ae32090d
Davis <zanxiaofeng@163.com>
2025-11-18 20:02:24 +0800

    update

---
+++ src/main/java/zxf/java/guava/CacheTests.java
@@ -0,0 +1,35 @@
+package zxf.java.guava;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.LoadingCache;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.concurrent.ExecutionException;
+
+public class CacheTests {
+    public static void main(String[] args) throws ExecutionException {
+        LoadingCache<Request, String> cache = CacheBuilder.newBuilder()
+                .maximumSize(1000)
+                .build(
+                        new com.google.common.cache.CacheLoader<Request, String>() {
+                            @Override
+                            public String load(Request key) {
+                                System.out.println("Loading - " + key);
+                                return key.toString();
+                            }
+                        }
+                );
+        System.out.println("cache.get: " + cache.get(new Request("abc", "123")));
+        System.out.println("cache.get: " + cache.get(new Request("abc", "123")));
+        System.out.println("cache.get: " + cache.get(new Request("ddd", "123")));
+        System.out.println("cache.get: " + cache.get(new Request("ddd", "123")));
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class Request{
+        private String path;
+        private String filename;
+    }
+}
