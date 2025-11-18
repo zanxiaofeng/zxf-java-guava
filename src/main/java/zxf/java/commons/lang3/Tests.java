12ad9892fcb1cbb122266d55b2d50e10a7eb5c99
Davis <zanxiaofeng@163.com>
2025-11-18 19:49:35 +0800

    update

--- src/main/java/zxf/java/commons/lang3/Tests.java
+++ src/main/java/zxf/java/commons/lang3/Tests.java
@@ -10,16 +10,22 @@ import java.io.IOException;
 
 public class Tests {
     public static void main(String[] args) {
+        System.out.println("RandomStringUtils testing");
         System.out.println(RandomStringUtils.random(12, true, false));
         System.out.println(RandomStringUtils.random(12, true, true));
 
+        System.out.println("ObjectUtils testing");
         System.out.println(ObjectUtils.allNotNull(12,23));
 
+        System.out.println("StringUtils testing");
         System.out.println(StringUtils.normalizeSpace(""));
 
-        System.out.println(BooleanUtils.toBoolean("On", "on", "off"));
+        System.out.println("BooleanUtils testing");
+        System.out.println(BooleanUtils.toBoolean("On"));
+        System.out.println(BooleanUtils.toBoolean("on", "on", "off"));
 
 
-        System.out.println(ExceptionUtils.getStackTrace(new RuntimeException(new IOException(new IllegalAccessException()))));
+        System.out.println("ExceptionUtils testing");
+        System.out.println(ExceptionUtils.getStackTrace(new RuntimeException("A", new IOException("B", new IllegalAccessException("C")))));
     }
 }
